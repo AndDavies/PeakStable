8d98f9a45568e6636a191337a0fe5228799c2981
madvaies <m.andrew.davies@gmail.com>
2025-01-10 15:44:01 +0100

    we have a stable cookie implementation. v9988

--- lib/supabaseServerClient.ts
+++
@@ -1,8 +0,0 @@
-// lib/supabaseServerClient.ts
-import { cookies } from "next/headers";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-
-export function createSupabaseServerClient() {
-  console.log("[supabaseServerClient.ts] Creating server Supabase client...");
-  return createServerComponentClient({ cookies });
-}
