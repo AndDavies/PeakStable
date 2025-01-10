13be797c2906ca180cca847984b726b3f2d42690
madvaies <m.andrew.davies@gmail.com>
2025-01-10 17:25:02 +0100

    we have a stable cookie implementation. v9988

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,9 +1,13 @@
 // lib/supabase.ts
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 console.log("[supabase.ts] Creating Supabase client (client-side). URL:", supabaseUrl);
+
+if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error('Missing Supabase URL or Key');
+  }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
